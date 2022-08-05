ec67a46918bb26a247e54a12609ff1cbd6b0adf8
Yashsva Sisodiya <yashsvasisodiya@Yashsvas-MacBook-Pro.local>
2022-08-05 12:28:18 +0530

    initial commit

---
+++ src/test/java/com/example/onboardingassign1/OnboardingAssign1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onboardingassign1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnboardingAssign1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
